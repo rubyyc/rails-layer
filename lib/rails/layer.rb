8c38914b3efe62028bc374a5a19998fcb54fa4a8
yuanchang <759337828@qq.com>
2019-12-01 10:26:24 +0000

    gem init

---
+++ lib/rails/layer.rb
@@ -0,0 +1,8 @@
+require "rails/layer/version"
+
+module Rails
+  module Layer
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/rails/layer/version.rb
@@ -0,0 +1,5 @@
+module Rails
+  module Layer
+    VERSION = "0.1.0"
+  end
+end
